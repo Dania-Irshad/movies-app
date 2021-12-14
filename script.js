3483f2569e59a96cbe4baf296efb6e1453d841ee
Dania Irshad <daniairshad@live.com>
2021-12-14 11:22:06 -0600

    add input validation

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const moviesArr = [];
-const ratingArr = [];
-
-$(document).ready(function(){
-    $(form).submit(function(e){
+$(document).ready(function () {
+    $(form).submit(function (e) {
         e.preventDefault();
-        if (!($('#table').length)){
-            $('<table id="table"></table>').insertAfter('br');
-            $thead = "<thead> <tr> <th>Movie Title</th> <th>Movie Rating</th> </tr> </thead>";
-            $('#table').append($thead);
-            $('<tbody>').insertAfter('thead');
+        if ($('#rating').val() >= 0 && $('#rating').val() <= 10 && $('#title').val().length > 2)
+        {
+            if (!($('#table').length)) {
+                $('<table id="table"></table>').insertAfter('br');
+                $thead = "<thead> <tr> <th>Movie Title</th> <th>Movie Rating</th> </tr> </thead>";
+                $('#table').append($thead);
+                $('<tbody>').insertAfter('thead');
+            }
+    
+            $('<tr>').appendTo('tbody')
+                .append(`<td>${$('#title').val()}</td>`)
+                .append(`<td>${$('#rating').val()}</td>`)
+                .append('<button>X</button>');
+            $('button').on('click', function () {
+                if ($(this).attr('id') !== 'submit') {
+                    $(this).parent().remove();
+                    if (!($('tbody tr').length)) {
+                        $('#table').remove();
+                    }
+                }
+            });
         }
-
-        $('<tr>').appendTo('tbody')
-        .append(`<td>${$('#title').val()}</td>`)
-        .append(`<td>${$('#rating').val()}</td>`)
-        .append('<button>X</button>');
+        else 
+        {
+            alert("PLEASE ENTER VALID INPUT");
+        }
+        
     });
 
 
