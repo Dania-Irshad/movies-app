debd8563b759420d1292e19cbc82aa5c869bfcab
Dania Irshad <daniairshad@live.com>
2021-12-14 14:09:58 -0600

    style table

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         if ($('#rating').val() >= 0 && $('#rating').val() <= 10 && $('#rating').val() !== '' && $('#title').val().length > 2)
         {
             if (!($('#table').length)) {
-                $('<table id="table"></table>').insertAfter('br');
+                $('<table id="table"></table>').appendTo('#table-container');
                 $thead = "<thead> <tr> <th>Movie Title</th> <th>Movie Rating</th> </tr> </thead>";
                 $('#table').append($thead);
                 $('<tbody>').insertAfter('thead');
@@ -13,7 +13,9 @@ $(document).ready(function () {
             $('<tr>').appendTo('tbody')
                 .append(`<td>${$('#title').val()}</td>`)
                 .append(`<td>${$('#rating').val()}</td>`)
-                .append('<button>X</button>');
+                .append('<button class="delete">X</button>');
+            $('#title').val('');
+            $('#rating').val('');
             $('button').on('click', function () {
                 if ($(this).attr('id') !== 'submit') {
                     $(this).parent().remove();
@@ -26,6 +28,8 @@ $(document).ready(function () {
         else 
         {
             alert("PLEASE ENTER VALID INPUT");
+            $('#title').val('');
+            $('#rating').val('');
         }
         
     });
